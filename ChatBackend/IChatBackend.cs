1d21399aef35672c68b317bab2fc346511780b33
Javier Escobar Espinoza <javieresc97@hotmail.com>
2018-05-05 16:43:49 -0500

    Initial commit

---
+++ ChatBackend/IChatBackend.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChatBackend
+{
+    [ServiceContract]
+    public interface IChatBackend
+    {
+        [OperationContract(IsOneWay = true)]
+        void DisplayMessage(CompositeType composite);
+
+        void SendMessage(string text);
+    }
+
+    [DataContract]
+    public class CompositeType
+    {
+        public CompositeType() { }
+        public CompositeType(string u, string m)
+        {
+            Username = u;
+            Message = m;
+        }
+
+        [DataMember]
+        public string Username { get; set; } = "Anonymous";
+
+        [DataMember]
+        public string Message { get; set; } = "";
+    }
+
+    public delegate void DisplayMessageDelegate(CompositeType data);
+}
